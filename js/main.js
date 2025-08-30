c4499d0742b73430167b660756fddf74fe0a3130
Amina <64805048+minalfatih@users.noreply.github.com>
2025-08-30 11:12:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,14 +8,18 @@ menuBtn.onclick = function () {
     overlay.className = "overlay";
     document.body.appendChild(overlay);
     document.querySelector("header .navbar").classList.add("active");
+    
+    document.querySelector("header .hamburger").classList.add("active");
+    
     setTimeout(() => {
       document.querySelectorAll('header .collapse ul li a').forEach(el => {
         el.addEventListener('click', () => {
-          menuBtn.classList.remove('active');
+          menuBtn.classList.remove('active')
           overlay.remove();
           document.querySelector('.collapse').classList.remove('show');
           document.querySelector("header svg").classList.remove("active");
-        })
+          document.querySelector("header .hamburger").classList.remove("active");
+          })
       });
     }, 500);
   } else {
@@ -23,9 +27,12 @@ menuBtn.onclick = function () {
       document.querySelector("header .navbar").classList.remove("active");
       document.querySelector(".overlay").remove();
       document.querySelector("header svg").classList.remove("active")
+      
+      document.querySelector("header .hamburger").classList.remove("active")
     }, 500);
   }
 };
+
 
 let number = 2240;
 document.getElementById("counter").textContent = number.toLocaleString();
